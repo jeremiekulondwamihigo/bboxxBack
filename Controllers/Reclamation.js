78d871d48f93b217167f49f9f317da2595c903c9
unknown <jeremie2018mihigo@gmail.com>
2023-11-29 21:33:57 +0200

    message

--- Controllers/Reclamation.js
+++ Controllers/Reclamation.js
@@ -1,165 +1,93 @@
-const asyncLab = require("async");
-const modelDemande = require("../Models/Demande");
-const modelReclamation = require("../Models/Reclamation");
-const { ObjectId } = require("mongodb");
-const _ = require("lodash");
+const asyncLab = require('async')
+const modelDemande = require('../Models/Demande')
+const modelReclamation = require('../Models/Reclamation')
+const { ObjectId } = require('mongodb')
+const _ = require('lodash')
 
 module.exports = {
   Reclamation: (req, res) => {
     try {
-      const { idDemande, message, sender, codeAgent } = req.body;
-
-      if (!idDemande || !message) {
-        return res.status(201).json("Error");
+      const { _id, message, sender, codeAgent } = req.body
+      if (!_id || !message || !sender || !codeAgent) {
+        return res.status(201).json('Error')
       }
       asyncLab.waterfall(
         [
           function (done) {
             modelReclamation
               .create({
-                message,
+                message,codeAgent,
                 sender,
-                idDemande,
+                code : new ObjectId(_id),
               })
               .then((response) => {
                 if (response) {
-                  done(null, response);
+                  done(response)
                 }
               })
               .catch(function (errr) {
+                console.log(errr)
                 if (errr) {
-                  return res.status(201).json("Try again");
+                  return res.status(201).json('Try again')
                 }
-              });
-          },
-          function (reponse, done) {
-            modelReclamation
-              .find({ idDemande: reponse.idDemande })
-              .then((response) => {
-                if (response) {
-                  done(null, response);
-                }
-              });
-          },
-          function (reponse, done) {
-            if (sender === "co") {
-              modelDemande
-                .findOneAndUpdate(
-                  { idDemande },
-                  { $set: { concerne: codeAgent } }
-                )
-                .then((result) => {
-                  if (result) {
-                    done(reponse);
-                  }
-                })
-                .catch(function (err) {
-                  console.log(err);
-                });
-            } else {
-              done(reponse);
-            }
+              })
           },
         ],
         function (result) {
-          return res.status(200).json(result);
-        }
-      );
+          return res.status(200).json(result)
+        },
+      )
     } catch (error) {
-      console.log(error);
-    }
-  },
-  ReadReclamation: (req, res) => {
-    try {
-      const { id } = req.params;
-
-      asyncLab.waterfall(
-        [
-          function (done) {
-            modelDemande
-              .aggregate([
-                { $match: { _id: new ObjectId(id) } },
-                {
-                  $lookup: {
-                    from: "conversations",
-                    localField: "idDemande",
-                    foreignField: "idDemande",
-                    as: "conversation",
-                  },
-                },
-                {
-                  $lookup: {
-                    from: "reponses",
-                    localField: "idDemande",
-                    foreignField: "idDemande",
-                    as: "reponse",
-                  },
-                },
-              ])
-              .then((response) => {
-                if (response) {
-                  done(response);
-                }
-              })
-              .catch(function (err) {
-                console.log(err);
-              });
-          },
-        ],
-        function (conversation) {
-          return res.status(200).json({
-            conversation: conversation[0].conversation,
-            reponse: conversation[0].reponse,
-          });
-        }
-      );
-    } catch (error) {
-      console.log(error);
+      console.log(error)
     }
   },
   ReadMessage: (req, res) => {
     try {
+      const {id} = req.params
       modelReclamation
         .aggregate([
           {
+            $match : {codeAgent : id}
+          },
+          {
             $lookup: {
-              from: "demandes",
-              localField: "idDemande",
-              foreignField: "idDemande",
-              as: "demandeId",
+              from: 'demandes',
+              localField: 'code',
+              foreignField: '_id',
+              as: 'demandeId',
             },
           },
           {
             $lookup: {
-              from: "reponses",
-              localField: "idDemande",
-              foreignField: "idDemande",
-              as: "reponseId",
+              from: 'reponses',
+              localField: 'code',
+              foreignField: '_id',
+              as: 'reponseId',
             },
           },
         ])
         .then((response) => {
-          return res.status(200).json(response);
-        });
+          return res.status(200).json(response)
+        })
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
   },
   MakeFalse: (req, res) => {
     try {
-      const { id } = req.params;
+      const { id } = req.params
       modelReclamation
         .findByIdAndUpdate(id, { $set: { valide: true } }, { new: true })
         .then((response) => {
           if (response) {
-            return res.status(200).json(response);
+            return res.status(200).json(response)
           }
         })
         .catch(function (err) {
-          console.log(err);
-        });
+          console.log(err)
+        })
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
   },
-};
+}
